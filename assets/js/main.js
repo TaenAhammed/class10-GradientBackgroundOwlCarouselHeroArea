07eb51b7078743ba0a99eb8663ad0fd402166bf4
TaenAhammed <32127459+TaenAhammed@users.noreply.github.com>
2018-03-26 17:56:29 +0600

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('.homepageSlider').owlCarousel({
+        items: 1,
+        loop: true,
+        autoplay: true,
+        nav: true,
+        navText: ['<i class="fa fa-long-arrow-left"></i>' , '<i class="fa fa-long-arrow-right"></i>']
+    });
+});
